ba94dfa8c7e1675dc9838a1203f1c89e3ec72bb0
Huzaifa Saleh <satstudent84@gmail.com>
2026-05-03 01:25:14 +0500

    feat: docker compose, nginx config, terraform, EC2 setup closes #5 #6 #7 #8

---
+++ terraform/main.tf
@@ -0,0 +1,70 @@
+erraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "phantasm_sg" {
+  name        = "phantasm-sg"
+  description = "PHANTASM security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3000
+    to_port     = 3003
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "phantasm" {
+  ami                    = "ami-0c02fb55956c7d316"
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.phantasm_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y docker.io docker-compose-plugin git
+    systemctl start docker
+    systemctl enable docker
+    usermod -aG docker ubuntu
+  EOF
+
+  tags = { Name = "phantasm-server" }
+}
+
+resource "aws_eip" "phantasm_ip" {
+  instance = aws_instance.phantasm.id
+}
+
+Create terraform/variables.tf:
+variable "aws_region" { default = "us-east-1" }
+variable "key_name" { description = "EC2 key pair name" }
+
+Create terraform/outputs.tf:
+output "public_ip" {
+  value = aws_eip.phantasm_ip.public_ip
+}
