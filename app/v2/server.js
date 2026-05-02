23ac87c019868c8091eb484fab1d26ec947e2a73
Misbah Ullah <misbahu094@gmail.com>
2026-05-02 23:34:38 +0500

    This fixes the typo errors suggested in #24 related to Issue #3

--- app/v2/server.js
+++ app/v2/server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/heatlh', (req,res) => {
+app.get('/health', (req,res) => {
     res.json ({ status: 'healthy', version: VERSION, color: 'green', requests: requestCount });    
 });
 
