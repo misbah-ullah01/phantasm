06fd76f00c69cfddac3953648e8a75fa74b3a415
AbdulRauf <mrrauf228@gmail.com>
2026-05-06 21:40:47 +0500

    feat: add AWS provider and default VPC data source configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,38 @@ data "aws_vpc" "default" {
   default = true
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_security_group" "phantasm" {
+  name        = "phantasm-security-group"
+  description = "Allow SSH, HTTP, and controller access"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ 
