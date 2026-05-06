2bfafdc8b268f44e440fbff3d76dc875c52d8fb6
Misbah Ullah <misbahu094@gmail.com>
2026-05-06 23:44:39 +0500

    refactor: update AWS instance configuration and clean up unused AMI data

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,21 +17,6 @@ data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_security_group" "phantasm" {
   name        = "phantasm-security-group"
   description = "Allow SSH, HTTP, and controller access"
@@ -71,8 +56,8 @@ resource "aws_security_group" "phantasm" {
 }
 
 resource "aws_instance" "phantasm" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
+  ami                    = "ami-091138d0f0d41ff90"  # Ubuntu 26.04 LTS (HVM, SSD)
+  instance_type          = "t3.micro"
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.phantasm.id]
 
